2d1f4cf4fe45931673240cb26596ff2ecb8b99ba
xrmethula <x.r.methula@gmail.com>
2022-08-03 16:26:17 +0200

    upades

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from selenium.webdriver.chrome.options import Options
 options = Options()
 options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 options.add_argument('--headless')
-driver = webdriver.Chrome(service=ChromeService(executable_path=os.environ.get("CHROMEDRIVER_PATH"), service_args=ChromeDriverManager().install()), options=options,)
+driver = webdriver.Chrome(service=ChromeService(executable_path=os.environ.get("CHROMEDRIVER_PATH"), service_args=[ChromeDriverManager().install()]), options=options,)
   
 driver.get('https://sssinstagram.com/')
 driver.save_screenshot('screenshot.png')
