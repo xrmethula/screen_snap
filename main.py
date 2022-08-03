a81456f025b504503cbbd2dbfaf1a3326aa90477
xrmethula <x.r.methula@gmail.com>
2022-08-03 17:29:47 +0200

    upades

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from selenium.webdriver.chrome.options import Options
 options = Options()
 options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 options.add_argument('--headless')
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-sh-usage')
+
 driver = webdriver.Chrome(executable_path=os.environ.get('CHROMEDRIVER_PATH'), options=options)
   
 driver.get('https://sssinstagram.com/')
