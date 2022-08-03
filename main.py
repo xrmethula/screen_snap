533ae5091c6635915c1bba21943b6f92caf01e0f
xrmethula <x.r.methula@gmail.com>
2022-08-03 15:22:10 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import os
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+
+options = Options()
+options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+options.add_argument('--headless')
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager(path=os.environ.get("CHROMEDRIVER_PATH")).install()), options=options,)
+  
+driver.get('https://sssinstagram.com/')
+driver.save_screenshot('screenshot.png')
+
+driver.quit()
