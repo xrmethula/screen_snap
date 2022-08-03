6a68a8f4036012b1b06b406b09c7caec3358ce3d
xrmethula <x.r.methula@gmail.com>
2022-08-03 17:36:07 +0200

    upades

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 
 options = Options()
@@ -7,8 +9,7 @@ options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 options.add_argument('--headless')
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-sh-usage')
-
-driver = webdriver.Chrome(executable_path=os.environ.get('CHROMEDRIVER_PATH'), options=options)
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options,)
   
 driver.get('https://sssinstagram.com/')
 driver.save_screenshot('screenshot.png')
